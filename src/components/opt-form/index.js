143caf7488368a12e0a6c8c3d263e6614dbaa50e
samx23 <skalexsong@gmail.com>
2021-06-07 19:52:45 +0700

    Create : opt-in form components

---
+++ src/components/opt-form/index.js
@@ -0,0 +1,22 @@
+import { Container, Input, Button, Text, Break } from "./styles/opt-form";
+
+const OptForm = ({ children, ...restProps }) => (
+  <Container {...restProps}>{children}</Container>
+);
+
+OptForm.Input = ({ children, ...restProps }) => <Input {...restProps} />;
+
+OptForm.Button = ({ children, ...restProps }) => (
+  <Button {...restProps}>
+    {children}
+    <img src="/images/icons/chevron-right.png" alt="Try Now" />
+  </Button>
+);
+
+OptForm.Text = ({ children, ...restProps }) => (
+  <Text {...restProps}>{children}</Text>
+);
+
+OptForm.Break = ({ ...restProps }) => <Break {...restProps} />;
+
+export default OptForm;
